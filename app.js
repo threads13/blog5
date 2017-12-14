cff169af5576bb9b88ed9f97addade0b200e0e2a
Jacob Hoke <jacob@threads13.com>
2017-12-14 00:13:12 -0500

    follow up commit

--- app.js
+++ app.js
@@ -5,9 +5,31 @@ const port = 3000;
 
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+// APP CONFIG
+mongoose.connect("mongodb://localhost/jacob_blog2");
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
+// SCHEMA SETUP
+var blogSchema = new mongoose.Schema({
+    author: String,
+    title: String,
+    desc: String,
+    posted: {type: Date, default: Date.now()} 
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+// 	author: "Jacob",
+// 	title: "Test data",
+// 	desc: "Here is some dummy data"
+// });
+
+// ROUTES
 // LANDING PAGE
 app.get("/", function(req, res){
 	res.render("landing");
@@ -15,12 +37,31 @@ app.get("/", function(req, res){
 
 // INDEX Route
 app.get("/blogs", function(req, res){
-	res.render("blogs");
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("index", {blogs: blogs});
+		}
+	});;
 });
 
 // NEW ROUTE
 app.get("/blogs/new", function(req, res){
 	res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+	// create blog
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			res.render("new");
+		} else {
+			res.redirect("/blogs");
+		}
+	});
+	// redirect
 });
 
 app.post("/blogs", function(req, res){
